bf55432e2718ba2206d15bd535459167502332ff
dac-bdair <162110991+dac-bdair@users.noreply.github.com>
2024-05-31 16:59:30 +0100

    Add files via upload

---
+++ 01-ReadWebcam.py
@@ -0,0 +1,19 @@
+import cv2
+frameWidth = 640
+frameHeight = 480
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10,150)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Result", img)
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+        breaka
